8d48be5e00a68d0e57ddfbe9a129d9332b693531
Arleca <carolsantos2002@gmail.com>
2021-06-15 19:37:10 -0300

    Implementação do método jogar da classe PC

--- src/main/PC.java
+++ src/main/PC.java
@@ -4,12 +4,28 @@ public class PC {
 
 	private Mapa jogoMapa;
 	private char letra = 'O';
-	
+
 	public PC(Mapa jogoMapa) {
 		this.jogoMapa = jogoMapa;
 	}
-	
+
 	public boolean jogar() {
+
+		//int linha = jogoMapa.sortear(0, 3);
+		//int coluna = jogoMapa.sortear(0, 3);
+		//boolean isPosicaoValida = jogoMapa.jogar(linha, coluna, letra);
+
+		//while (!isPosicaoValida) {
+			//int linha = jogoMapa.sortear(0, 3);
+			//int coluna = jogoMapa.sortear(0, 3);
+			//isPosicaoValida = jogoMapa.jogar(linha, coluna, letra);
+		//}
+
+		//if (jogoMapa.verificarGanhador(letra)) {
+			//System.out.println("... PC GANHOU!");
+			//return true;
+		//} else {
+				//return false;
 		return true;
 	}
 }
