e808a3b9f75766ac0ac8f80e98df148105d20a39
nicole-taufenbach <ntaufenbach@hotmail.com>
2021-06-20 22:02:30 -0300

    adicionado do while no jogador

--- src/main/Jogador.java
+++ src/main/Jogador.java
@@ -13,28 +13,26 @@ public class Jogador {
 
 	public boolean jogar(Scanner teclado) {
 		letra = 'X';
-		boolean isJogadaValida = true;
-		
-		System.out.println("Jogador ..");
-		System.out.print("	linha: ");
-		int linha = teclado.nextInt();
-		System.out.print("	coluna: ");
-		int coluna = teclado.nextInt();
-		
-		if (linha >= 3 || linha < 0 || coluna >= 3 || coluna < 0) 
-			isJogadaValida = false;
-		else if ((mapa.jogar(linha, coluna, letra)) == false) 
-			isJogadaValida = false;
-		
-		while (!isJogadaValida) {
+		int linha, coluna;
+		boolean isValido = true;
+
+		do {
 			System.out.println("Jogador ..");
 			System.out.print("	linha: ");
 			linha = teclado.nextInt();
 			System.out.print("	coluna: ");
 			coluna = teclado.nextInt();
-			isJogadaValida = mapa.jogar(linha, coluna, letra);
-			break;
-		}
+			
+			if (linha >= 3 || linha < 0 || coluna >= 3 || coluna < 0) {
+				isValido = false;
+			} else if (mapa.jogar(linha, coluna, letra) == false) {
+				System.out.println(" posição inválida!");
+				isValido = false;
+			} else {
+				isValido = true;
+			}
+			
+		} while (!isValido);
 
 		if (mapa.verificarGanhador(letra)) {
 			System.out.println("... Jogador GANHOU!");
@@ -42,6 +40,6 @@ public class Jogador {
 		} else {
 			return false;
 		}
-		
+
 	}
 }
